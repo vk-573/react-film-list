cbda9fb9310319c4abc2fe5ef182fe5a0a8986cb
Kiryk <valentin.kiryk@gmail.com>
2020-10-25 17:34:44 +0100

    feat(Film) film component

--- src/components/film/film.js
+++ src/components/film/film.js
@@ -1,15 +1,39 @@
+import { useState } from 'react';
+
 import PropTypes from 'prop-types';
 import MovieIcon from '@material-ui/icons/Movie';
+import ThumbUp from '@material-ui/icons/ThumbUp';
+import ThumbDown from '@material-ui/icons/ThumbDown';
+import { LinearProgress } from '@material-ui/core';
 
 import "./film.scss";
 
 export const Film = (props) => {
+  const [liked, setLiked] = useState(null);
+  const [likes, setLikes] = useState(props.likes);
+  const [dislikes, setDislikes] = useState(props.dislikes);
   const onLike = () => {
-    console.log("like");
+    if (liked === true) {
+      setLikes(likes - 1);
+      setLiked(null);
+      return;
+    } else if (liked === false) {
+      setDislikes(dislikes - 1);
+    }
+    setLikes(likes + 1);
+    setLiked(true);
   }
 
   const onDislike = () => {
-    console.log("dislike");
+    if (liked === false) {
+      setLiked(null);
+      setDislikes(dislikes - 1);
+      return;
+    } else if (liked === true) {
+      setLikes(likes - 1);
+    }
+    setDislikes(dislikes + 1);
+    setLiked(false);
   }
 
   console.log("props:", props);
@@ -19,12 +43,27 @@ export const Film = (props) => {
         <MovieIcon/>
       </div>
       <div className="details">
-        {props.title}
-        {props.category}
+        <div className="title">
+          {props.title}
+        </div>
+        <div className="category">
+          {props.category}
+        </div>
       </div>
-      <div>
-        {props.likes}
-        {props.dislikes}
+      <div className="ratio-p">
+        <dov className="icons">
+          <div>
+            <ThumbUp className={liked === true ? 'liked' : ''} onClick={onLike} />
+            <span className="likes">{likes}</span>
+          </div>
+          <div>
+            <ThumbDown className={liked === false ? 'disliked' : ''} onClick={onDislike} />
+            <span className="dislikes">{dislikes}</span>
+          </div>
+        </dov>
+        <div className="ratio-bar">
+          <LinearProgress variant="determinate" value={(likes / (likes + dislikes)) * 100} />
+        </div>
       </div>
     </div>
   );
